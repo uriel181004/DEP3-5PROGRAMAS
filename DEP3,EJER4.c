04502ce56283a2e697a431b19311601959f3480c
uriel181004 <82549982+uriel181004@users.noreply.github.com>
2021-05-19 21:52:25 -0500

    Add files via upload

---
+++ DEP3,EJER4.c
@@ -0,0 +1,26 @@
+/*Dep3_Prog4:
+ Verificar si es vocal o letra*/
+ #include <stdio.h>
+
+ int main()
+ {
+    char letra;
+    printf("\n Introduce una letra");
+    scanf("%c", &letra);
+    
+    if( (letra == 'a' || letra == 'A') ||
+        (letra == 'e' || letra == 'E') ||
+        (letra == 'i' || letra == 'I') ||
+        (letra == 'o' || letra == 'O') ||
+        (letra == 'u' || letra == 'U')
+
+      )
+    {
+       printf("\n ES UNA VOCAL");
+    }
+    else
+    {
+       printf("\n NO ES VOCAL");
+    }
+    return 0;
+ }
